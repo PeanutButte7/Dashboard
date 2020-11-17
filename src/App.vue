ac58c82b5fc19f5f8af0b909230c4c386c6f94e3
PeanutButte7 <adam.barta007@gmail.com>
2020-11-18 00:14:02 +0100

    Vue config

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mx-auto py-20 text-primary">
+    <h1 class="text-10xl font-semibold">Monday</h1>
+    <a href="https://updown.io/checks">test</a>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    colors: {
+      primary: '#F7FAFC',
+      accent: '',
+      background: '#1B1C2F'
+    },
+    extend: {
+      fontSize: {
+        '4xl': '2.5rem',
+        '10xl': '7rem'
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
