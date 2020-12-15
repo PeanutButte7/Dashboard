4bc01f5f87111de3bd22711e61f01eb4b0e63c34
PeanutButte7 <adam.barta007@gmail.com>
2020-12-15 21:43:57 +0100

    Added tooltip to server status

--- src/components/ServerStatus.vue
+++ src/components/ServerStatus.vue
@@ -1,7 +1,11 @@
 <template>
     <div id="serverStatus" class="inline-block mr-5">
-        <img v-if="!data.down" src="../assets/check-circle.svg" class="inline-block mb-1">
-        <img v-else src="../assets/x-circle.svg" class="inline-block mb-1">
+        <p class="tooltip inline-block">
+            <img v-if="!data.down" src="../assets/check-circle.svg" class="inline-block mb-1">
+            <img v-else src="../assets/x-circle.svg" class="inline-block mb-1">
+            <span v-if="!data.down" class="tooltip-text bg-accent p-3 -mt-10 -ml-6 rounded">Apdex: {{ data.apdex_t }}</span>
+            <span v-else class="tooltip-text bg-accent p-3 -mt-10 -ml-6 rounded">Down since: {{ data.down_since }}</span>
+        </p>
         <a :href="data.url" class="text-xl ml-3">{{ data.alias }}</a>
     </div>
 </template>
@@ -17,3 +21,16 @@
         }
     }
 </script>
+
+<style scoped>
+    .tooltip .tooltip-text {
+        visibility: hidden;
+        text-align: center;
+        padding: 2px 6px;
+        position: absolute;
+        z-index: 100;
+    }
+    .tooltip:hover .tooltip-text {
+        visibility: visible;
+    }
+</style>
